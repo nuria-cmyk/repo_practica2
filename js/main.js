fc5914d097033ad5dd6b2bce33d033825879af8d
nuria <nuria.fernandez.val@gmail.com>
2024-10-26 20:17:47 +0200

    Pequeños cambios en html y css, como los hover... Creación de las funciones de printOne y PrintAll para poder imprimir las planticas :)

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const seccionPlantas = document.querySelector('#plantas')
+
+function printOnePlant(plant, dom) {
+
+    const article = document.createElement('article')
+    const figure = document.createElement('figure')
+    const imagen = document.createElement('img')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('h4')
+    const h4 = document.createElement('h4')
+    const botonanadircarrito = document.createElement('button')
+
+    imagen.src = plant.imagen
+    imagen.alt = plant.nombre
+    h3.textContent = plant.nombre
+    p.textContent = plant.descripcion
+    h4.textContent = `Precio: ${plant.precio}€`
+    botonanadircarrito.id = 'anadircarrito'
+    botonanadircarrito.textContent = 'Añadir al carrito'
+
+    figure.appendChild(imagen)
+    article.append(figure, h3, p, h4, botonanadircarrito)
+    dom.appendChild(article)
+}
+
+
+function printAllPlants(list, dom) {
+    list.forEach(plant => printOnePlant(plant, dom))
+}
+printAllPlants(plantas, seccionPlantas)
