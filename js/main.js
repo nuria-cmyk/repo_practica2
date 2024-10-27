5a48fa9f026e7a3875a5938f625059314221c386
Fran Álvarez <franfm3@gmail.com>
2024-10-27 12:36:03 +0100

    Creamos paginación y funciona, pero está pendiente de revisión (variables y eventos)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const seccionPlantas = document.querySelector('#plantas')
+const itemsPerPage = 20;
+let currentPage = 1;
 
 function printOnePlant(plant, dom) {
 
@@ -22,9 +24,32 @@ function printOnePlant(plant, dom) {
     article.append(figure, h3, p, h4, botonanadircarrito)
     dom.appendChild(article)
 }
-
-
 function printAllPlants(list, dom) {
-    list.forEach(plant => printOnePlant(plant, dom))
+    dom.innerHTML = '';
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const plantsToShow = list.slice(start, end);
+    plantsToShow.forEach(plant => printOnePlant(plant, dom));
+
 }
+/* const itemsPerPage = 20;
+let currentPage = 1; */
+function nextPage(list) {
+    if (currentPage < Math.ceil(list.length / itemsPerPage)) {
+        currentPage++;
+        printAllPlants(list, seccionPlantas);
+    }
+}
+
+function prevPage(list) {
+    if (currentPage > 1) {
+        currentPage--;
+        printAllPlants(list, seccionPlantas);
+    }
+}
+
+document.querySelector('#paginacion button:first-child').addEventListener('click', () => prevPage(plantas));
+document.querySelector('#paginacion button:last-child').addEventListener('click', () => nextPage(plantas));
+
+
 printAllPlants(plantas, seccionPlantas)
